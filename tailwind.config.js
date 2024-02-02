660b78b70368d696dcbea1a8741eb64964a63526
m.uppenkamp <m.uppenkamp@babiel.com>
2024-02-02 18:56:29 +0100

    ✨ Adapt heights and widths

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 const { join } = require("path");
 const { createGlobPatternsForDependencies } = require('@nrwl/angular/tailwind');
 
+const minMaxWidth = {
+  screen: '100vi',
+  body: '30em',
+}
+
+const minMaxHeight = {
+  screen: '100vb',
+}
+
 module.exports = {
+  plugins: [
+    require('tailwindcss-logical'),
+    require('@mertasan/tailwindcss-variables'),
+  ],
   content: [
     join(__dirname, 'src/**/!(*.spec).{ts,html}'),
     // below function will identify the dependencies of the application and return the glob patterns for them
@@ -11,6 +24,11 @@ module.exports = {
     // see https://blog.nrwl.io/set-up-tailwind-css-with-angular-in-an-nx-workspace-6f039a0f4479 for additional information
     ...createGlobPatternsForDependencies(__dirname),
   ],
+  // If you want to support toggling dark mode manually instead of relying on the operating system preference,
+  // use the class strategy instead of the media strategy.
+  // If 'class' is selected dark:...-classes applied whenever a 'dark' class is present earlier in the HTML tree
+  // see https://tailwindcss.com/docs/dark-mode
+  darkMode: 'media',
   theme: {
     extend: {
       colors: {
@@ -19,12 +37,15 @@ module.exports = {
         'violet': '#E788EB',
         'tropical-indigo': '#AA80F2',
         'cornflower-blue': '#7295F7',
-        'mint-cream': '#F1F5ED'
-      }
+        'snow': '#f7f4f6',
+        'light-green': '#A6E995',
+      },
+      spacing: {
+      },
+      minWidth: minMaxWidth,
+      maxWidth: minMaxWidth,
+      minHeight: minMaxHeight,
+      maxHeight: minMaxHeight,
     },
   },
-  plugins: [
-    require('tailwindcss-logical'),
-    require('@mertasan/tailwindcss-variables'),
-  ],
 }
