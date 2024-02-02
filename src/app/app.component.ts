35eee57354299540d09a0f9351b2b7304adf76c4
m.uppenkamp <m.uppenkamp@babiel.com>
2024-02-02 18:57:49 +0100

    ✨ Add poll with answers to map through and functionality for buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Poll } from './polls.type';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,47 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  /**
+   * Poll contain question and answers
+   */
+  public poll: Poll = {
+    question: 'Mit den Winterhärtezonen von 1a bis 13b lässt sich herausfinden ...?',
+    answers: [
+      {
+        text: '... welches Frostschutzmittel für ein Fahrzeug geeignet ist',
+        voted: 0,
+        color: 'tropical-indigo'
+      },
+      {
+        text: '...wie der Heizkessel einer Gasheizung eingestellt werden muss',
+        voted: 0,
+        color: 'violet'
+      },
+      {
+        text: '... welche Tiefsttemperatur Pflanzen ohne Schäden überstehen können',
+        voted: 0,
+        color: 'lavender-pink'
+      },
+    ]
+  };
+
+  /**
+   * Contains the index of the answer
+   */
+  public votedFor: number | null = null;
+
+  /**
+   * Workflow when someone votes for an answer
+   * @param index of the answer
+   */
+  public vote(index: number): void {
+    const answer = this.poll.answers[index];
+    answer.voted += 1;
+    this.votedFor = index;
+  }
+
+  public convertToAlphabet(index: number): string {
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    return alphabet[index] ?? index.toString();
+  }
 }
